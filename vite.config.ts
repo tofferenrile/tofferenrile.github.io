befae62ccfa1547d2eefb4019f67ef816a90530d
tofferenrile <toffer.enrile0@gmail.com>
2026-04-29 23:03:42 +0800

    chore: prepare dist files

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig, loadEnv } from "vite";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
   return {
+    base: "/tofferenrile.github.io/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
